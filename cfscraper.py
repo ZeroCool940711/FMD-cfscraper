8188bfb8d5842da01405c7ac484b6ee276d8c03c
ZeroCool <ZeroCool940711@users.noreply.github.com>
2019-04-30 08:30:08 -0700

    Update cfscraper.py

--- cfscraper.py
+++ cfscraper.py
@@ -1,13 +1,10 @@
-import cfscrape, sys
+import cloudscraper, sys, requests
 
 url = sys.argv[1]
 
-scraper = cfscrape.create_scraper()  # returns a CloudflareScraper instance
-
-if "http" or "https" not in scraper:
-    url = "http://"+ url
-    
-tokens, user_agent = cfscrape.get_tokens(url)
+scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+ 
+tokens, user_agent = scraper.get_tokens(url)
 
 # print the cloudflare tokens and user_agent used
 print tokens, user_agent
